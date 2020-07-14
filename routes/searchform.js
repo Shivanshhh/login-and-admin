96b81dfd72e37312501c76d48edbd835a872e4e6
Shivanshhh <65827695+Shivanshhh@users.noreply.github.com>
2020-07-14 12:12:29 +0530

    Update searchform.js

--- routes/searchform.js
+++ routes/searchform.js
@@ -9,13 +9,14 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  const name1 = req.body.search;
-  const db = await Login.find({});
-
-  const names = db
-    .map((element) => (element.name))
-    .filter((name) => (name.startsWith(name1)));
-  res.render('searchform', { results: names });
+  const regex = new RegExp(`.*${req.body.search}.*`);
+  const db = await Login.find({ name: regex });
+  const db1 = db.map((element) => (element.name));
+  //   const names = db
+  //     .map((element) => (element.name))
+  //     .filter((name) => (name.startsWith(name1)));
+  console.log(db1);
+  res.render('searchform', { results: db1 });
 });
 
 module.exports = router;
